32afa3ba605a955cb493dce51ca0eef04e3df93b
Oderlan <oderlanfreire@gmail.com>
2023-10-28 16:07:35 -0300

    Adicionar arquivos de projeto.

---
+++ SitemaDeTarefas/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SitemaDeTarefas.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
